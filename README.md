0aff9b62a29c0cf71883394f8ff2aca959aedb45
ktunebe <ktunebe@gmail.com>
2024-03-18 00:20:26 -0500

    Initial commit - project mostly complete

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # random-student-selector
+
+## Description
+
+This is a project for UW bootcamp in which we needed a way to randomly select students during class and avoid repeating the same selection until everyone had been called on.
+
+## Installation
+
+N/A
+
+## Usage
+
+On clicking the "Select Random Student" button, a function will run to select a student from the class list. It will then add a count to their called on total, which will prevent them from being called on again. Once every student has been called, the counts reset.
+
+Link to deployed application: 
+
+![Screenshot of the Random Student Selector page]()
+
+## Credits
+
+- Fonts
+    - "Diediedie" Designed by Mike Emory. ©2001 Static Type Foundry. Downloaded from https://www.1001fonts.com/
+    - "Hothead" Hothead © 2002 Brain Eaters Font Company. Downloaded from https://www.1001fonts.com/
+
+- Color Palette obtained free from https://coolors.co/
+
+## License
+
+N/A
---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+// Array
+var students = ['Veysel', 'Jake', 'Nick', 'Kent', 'Caleb', 'Parker', 'Tom', 'Meg', 'Larry', 'Ryan'];
+// Variables
+let calledOn = 0;
+let studentList = [];
+
+// Select button
+const button = document.querySelector("#button");
+const winner = document.querySelector("#winner");
+const announcement = document.querySelector("#announcement");
+let drumRoll = '<img src="assets/images/drum_1f941.gif" alt="image description">';
+
+// Function to create new student object
+const Student = function(studentName, calledOn) {
+    this.studentName = studentName;
+    this.calledOn = calledOn;
+}
+
+// Create array of Student objects by looping through the array of student strings
+function createStudentList()  {
+    for (let i = 0; i < students.length; i++) {
+        studentName = students[i];
+        calledOn = 0;
+        student = new Student(studentName, calledOn);
+        studentList.push(student);
+    }
+    return studentList;
+}
+
+
+
+// Function to call random student, exlcuding ones who have been called and resetting count if all students have been called
+function getRandomStudent(array) {
+    const randomIndex = Math.floor( Math.random() * array.length );
+    let randomStudent = array[randomIndex];
+    const allCalled = array.every(obj => obj.calledOn > 0);
+    function displayAnnouncement() {
+        announcement.textContent = "ThE lucky student is: ";
+        winner.textContent = `${randomStudent.studentName}`
+    }
+    if (allCalled) {
+        array.forEach((randomStudent) => randomStudent.calledOn = 0);
+        getRandomStudent(array);
+    } else if (randomStudent.calledOn === 0) {
+        winner.textContent = "";
+        announcement.innerHTML = `${drumRoll}`;
+        setTimeout(displayAnnouncement, 1800);
+        randomStudent.calledOn++;
+    } else if (randomStudent.calledOn > 0) {
+        getRandomStudent(array);
+    } 
+}
+
+createStudentList();
+
+button.addEventListener('click', function e() {
+    getRandomStudent(studentList);
+    console.log(studentList);
+});
+
+
+
+
